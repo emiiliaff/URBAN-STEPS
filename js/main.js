a4492f806495b79d8cd3ca09fbc49c58ee829dce
emiliafripp <emiliafripp.c@gmail.com>
2024-05-23 13:55:20 -0300

    arrays, metodos y objeto

--- js/main.js
+++ js/main.js
@@ -1,56 +1,63 @@
 function saludar(nombre, apellido) {
-   console.log("Hola, " + nombre + " " + apellido);
-   alert("Hola, " + nombre + " " + apellido);
+  console.log("Hola, " + nombre + " " + apellido);
+  alert("Hola, " + nombre + " " + apellido);
 }
 
 let nombreIngresado;
 let apellidoIngresado;
 
 do {
-    nombreIngresado = prompt ("Ingresa tu nombre");
-    apellidoIngresado = prompt("Ingresa tu apellido");
-} while (nombreIngresado === "" || apellidoIngresado === "")
+  nombreIngresado = prompt("Ingresa tu nombre");
+  apellidoIngresado = prompt("Ingresa tu apellido");
+} while (nombreIngresado === "" || apellidoIngresado === "");
+
 saludar(nombreIngresado, apellidoIngresado);
 
+// Objetos con precios para cada modelo
+let modelos = [
+  { id: 1, nombre: 'Modelo1', precio: 100 },
+  { id: 2, nombre: 'Modelo2', precio: 200 },
+  { id: 3, nombre: 'Modelo3', precio: 300 }
+];
 
-// Variables con precios para cada modelo
-let precioModelo1 = 100;
-let precioModelo2 = 200;
-let precioModelo3 = 300;
-
-let precioTotal = 0;
+let carrito = [];
 
 // Función para mostrar las opciones al usuario
 function mostrarOpciones() {
-  alert("Selecciona un modelo:\n" +
-        "1. Modelo1 - $" + precioModelo1 + "\n" +
-        "2. Modelo2 - $" + precioModelo2 + "\n" +
-        "3. Modelo3 - $" + precioModelo3);
+  let mensaje = "Selecciona un modelo:\n";
+  modelos.forEach(modelo => {
+      mensaje += modelo.id + ". " + modelo.nombre + " - $" + modelo.precio + "\n";
+  });
+  alert(mensaje);
 }
 
-function calcularTotal(opcionElegida) {
-  switch (opcionElegida) {
-    case "1":
-      return precioModelo1;
-    case "2":
-      return precioModelo2;
-    case "3":
-      return precioModelo3;
-    default:
-      return 0;
-  }
+function calcularTotal() {
+  let total = 0;
+  carrito.forEach(modelo => {
+      total += modelo.precio;
+  });
+  return total;
 }
 
 do {
-  if (precioTotal === 0) {
-    mostrarOpciones();
+  if (carrito.length === 0) {
+      mostrarOpciones();
   } else {
-    alert("Precio total hasta el momento: $" + precioTotal);
+      alert("Precio total hasta el momento: $" + calcularTotal());
   }
-  
+
   let opcionElegida = prompt("Ingresa el número correspondiente al modelo que deseas comprar:");
-  precioTotal += calcularTotal(opcionElegida);
+  let modeloSeleccionado = modelos.find(modelo => modelo.id == opcionElegida);
+  
+  if (modeloSeleccionado) {
+      carrito.push(modeloSeleccionado);
+  } else {
+      alert("Opción no válida. Por favor, selecciona un modelo de la lista.");
+  }
 } while (confirm("¿Deseas elegir otro modelo?"));
+
+let precioTotal = calcularTotal();
 
 alert("El precio total es: $" + precioTotal);
 console.log("El precio total es: $" + precioTotal);
+
